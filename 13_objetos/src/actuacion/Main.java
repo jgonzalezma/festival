9c8bb1a3df025cbfcf881640108b7acd8cbcd38d
ik012982i9 <ik012982i9@206-09-HZ108349.H012982.NET>
2017-12-18 08:27:34 +0100

    añadimos actuacion el reno renardo

--- 13_objetos/src/actuacion/Main.java
+++ 13_objetos/src/actuacion/Main.java
@@ -64,6 +64,7 @@ public class Main {
 		festival.mostrasInfo();
 		
 		festival.addActuacion("berri txarrak", 20);
+		festival.addActuacion("El reno renardo", 100);
 		
 		festival.mostrasInfo();
 	}
