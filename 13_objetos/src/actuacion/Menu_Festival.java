ed506d05455c74b7fe8dc20a062dfb7e25e3f862
ik012982i9 <ik012982i9@206-09-HZ108349.H012982.NET>
2017-12-19 08:48:52 +0100

    Añadimos el menu de festival

---
+++ 13_objetos/src/actuacion/Menu_Festival.java
@@ -0,0 +1,15 @@
+package actuacion;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Menu_Festival {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File f = new File("D:/Josu/Programación/actuacionesf.txt");
+		Scanner fich = new Scanner(f);
+		
+	}
+
+}
